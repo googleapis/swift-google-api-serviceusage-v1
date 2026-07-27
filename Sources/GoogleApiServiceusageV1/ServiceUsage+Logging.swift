bedee36fb3d3621b584437dd89500440e13c35b9
Carlos O'Ryan <coryan@google.com>
2026-07-27 13:05:32 -0400

    chore: update librarian circa 2026-07-24 (#20)
    Update the librarian version. In this version the `gcp.artifact.id`
    logging attribute is set to the package name, as it should be.

--- Sources/GoogleApiServiceusageV1/ServiceUsage+Logging.swift
+++ Sources/GoogleApiServiceusageV1/ServiceUsage+Logging.swift
@@ -31,7 +31,7 @@ extension Clients {
 
     public init(_ inner: any ServiceUsageStub, logger: Logger) {
       var logger = logger
-      logger[metadataKey: "gcp.artifact.id"] = "GoogleApiServiceusageV1"
+      logger[metadataKey: "gcp.artifact.id"] = "google-api-serviceusage-v1"
       logger[metadataKey: "gcp.client.service"] = "serviceusage"
       logger[metadataKey: "gcp.experimental.swift.client"] = "ServiceUsage"
       self.inner = inner
